94a6063d680ced74a6498dd157c704fd77dbdb16
Salaimuniselvam <salaimuniselvam7@gmail.com>
2021-05-13 21:41:31 +0530

    completed the project

---
+++ function_to_toggle.js
@@ -0,0 +1,7 @@
+
+    var searchBox = document.getElementById("searchBox");
+var googleIcon = document.getElementById("googleIcon");
+
+googleIcon.onclick = function(){
+     searchBox.classList.toggle("active");
+}
